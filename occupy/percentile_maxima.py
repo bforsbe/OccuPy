7081e9c3057fc10f05d5c2e40473fe085d3c72e3
bforsbe <bj.o.forsberg@gmail.com>
2022-08-20 09:07:08 +0100

    Add uniform distr. to tau script

--- occupy/percentile_maxima.py
+++ occupy/percentile_maxima.py
@@ -7,11 +7,14 @@ def right_of_x(x):
 def left_of_x(x):
     return 0.5 * (1+special.erf(x/np.sqrt(2)))
 
+def left_of_x_uni(x):
+    return np.clip(x,0.0,1.0)
+
 # Test n_V = 1,2,...,n_max as the number of draws from the distribution
 n_max = 10
 
 # test each value of n_V this many times for significance in the distribution of max-values
-n_samples = 1000000
+n_samples = 10000
 
 # prob thresholds
 p = [0.5,0.6,0.7,0.8,0.9, 0.95, 0.99]
@@ -39,15 +42,29 @@ out=np.zeros(n_max)
 #
 #     print(f'{i+1} \t {p_str}')
 
-print(f'n \t tau \t p')
-for n in [1,7,33,123,257,515,925,1419,2109,3071]:
-    max_values = np.zeros(n_samples)
+n_v_max = 50
+accuracy=0.001
+t=np.zeros((n_v_max,2),dtype=float)
+print(f'n \t tau_norm \t p_norm \t tau_uni \t p_uni')
+for n in np.arange(n_v_max)+1:    # [1,7,33,123,257,515,925,1419,2109,3071]:
+    max_values_norm = np.zeros(n_samples)
+    max_values_uni = np.zeros(n_samples)
     for j in np.arange(n_samples):
-        max_values[j] = np.max(np.random.randn(n))
-    tau=1.0
-    p = 0
-    while p < tau and tau > 0.01:
-        tau -= 0.0001
-        p = left_of_x(np.percentile(max_values,(1-tau)*100))
-    print(f'{n} \t {tau:.4f} \t {p:.4f}')
+        max_values_norm[j] = np.max(np.random.randn(n))
+        max_values_uni[j] = np.max(np.random.rand(n))
+    tau_norm=1.0
+    p_norm = 0
+    while p_norm < tau_norm and tau_norm > 0.01:
+        tau_norm -= accuracy #0.0001
+        p_norm = left_of_x(np.percentile(max_values_norm, (1 - tau_norm) * 100))
+    t[n-1,0]=tau_norm
+
+    tau_uni = 1.0
+    p_uni = 0
+    while p_uni < tau_uni and tau_uni > 0.01:
+        tau_uni -= accuracy #0.0001
+        p_uni = left_of_x_uni(np.percentile(max_values_uni, (1 - tau_uni) * 100))
+    t[n-1,1]=tau_uni
+
+    print(f'{n} \t {tau_norm:.4f} \t {p_norm:.4f} \t {tau_uni:.4f} \t {p_uni:.4f}')
     #print('-------------------------------------------')
