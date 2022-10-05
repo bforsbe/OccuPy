36dcce7d3ab3b084b499ed5396a97573a8729d93
bforsbe <bj.o.forsberg@gmail.com>
2022-10-05 11:54:58 +0100

    all resources included at pip install

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ import setuptools
 setuptools.setup(
     use_scm_version={"write_to": "occupy/_version.py"},
     entry_points={
-        'console_scripts': ['occupy=occupy.occupy:app','occupy_gui=occupy.occupy:app_gui']}
+        'console_scripts': ['occupy=occupy.occupy:app','occupy_gui=occupy.occupy:app_gui']},
+    include_package_data=True
 )
