5f86ee0533615b32b0f2830d34527be98bcfba14
bforsbe <bj.o.forsberg@gmail.com>
2022-10-06 13:20:11 +0100

    Fix build and deploy

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 import setuptools
 
 setuptools.setup(
-    use_scm_version={"write_to": "occupy/_version.py"},
+    use_scm_version={"write_to": "occupy_lib/_version.py"},
     entry_points={
-        'console_scripts': ['occupy=occupy.occupy:app','occupy_gui=occupy.occupy:app_gui']},
+        'console_scripts': ['occupy=occupy_lib.occupy:app','occupy_gui=occupy_lib.occupy:app_gui']},
     include_package_data=True
 )
